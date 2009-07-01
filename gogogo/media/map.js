ae5210e070998242bada8e6e82b665426374fc53
Ben Lau <benlau@visionware.com.hk>
2009-07-02 00:09:03 +0800

    Added gogogo/media/map.js - A javascript custom GMap2 class

---
+++ gogogo/media/map.js
@@ -0,0 +1,15 @@
+/*
+ * gogogo.map
+ *
+ *  Licensed under Affero GPL v3 (www.fsf.org/licensing/licenses/agpl-3.0.html )
+ */
+
+gogogo.Map = function (container){
+	GMap2.call(this,container);
+	this.setUIToDefault();
+	
+	this.setCenter(new GLatLng(gogogo.DEFAULT_LOCATION[0],gogogo.DEFAULT_LOCATION[1])
+		, gogogo.DEFAULT_ZOOM);
+};
+
+$.extend(gogogo.Map,GMap2);
--- gogogo/settings.py
+++ gogogo/settings.py
@@ -1,9 +1,26 @@
 from ragendja.settings_post import settings
+from django.conf import settings as django_settings
+
+def setting_converter(**kwargs):
+	"""
+	Generate default setting for gogogo javascript
+	"""
+	ret = """
+var gogogo = {}
+gogogo.DEFAULT_ZOOM=%d;
+gogogo.DEFAULT_LOCATION = [%f,%f];
+""" % (django_settings.GOGOGO_DEFAULT_ZOOM,
+	django_settings.GOGOGO_DEFAULT_LOCATION[0],
+	django_settings.GOGOGO_DEFAULT_LOCATION[1])
+	return ret
 
 settings.add_app_media('combined-%(LANGUAGE_CODE)s.js',
     'gogogo/jquery-ui-1.7.1.custom.min.js',
     'gogogo/jquery.layout.js',
-
+    
+    #Gogogo javascript
+    setting_converter,
+	'gogogo/map.js',
     # ...
 )
 
