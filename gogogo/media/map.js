d2caa447a90354a6f487e54386f2a2c2a1e4f833
Ben Lau <benlau@visionware.com.hk>
2009-08-10 11:16:51 +0800

    Deprecate gogogo.Map class , replaced by gogogo.mapSetup() - func to initialize GMap2 object

--- gogogo/media/map.js
+++ gogogo/media/map.js
@@ -4,6 +4,8 @@
  *  Licensed under Affero GPL v3 (www.fsf.org/licensing/licenses/agpl-3.0.html )
  */
 
+/* Deprecated due to IE issue.
+ 
 gogogo.Map = function (container){
 	GMap2.call(this,container);
 	this.setUIToDefault();
@@ -13,3 +15,11 @@ gogogo.Map = function (container){
 };
 
 $.extend(gogogo.Map,GMap2);
+*/
+
+gogogo.mapSetup = function(map){
+	map.setUIToDefault();
+	
+	map.setCenter(new GLatLng(gogogo.DEFAULT_LOCATION[0],gogogo.DEFAULT_LOCATION[1])
+		, gogogo.DEFAULT_ZOOM);	
+}
