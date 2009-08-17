4a80325a63be4b190eff0b7449c7cfc203342fcd
Ben Lau <benlau@visionware.com.hk>
2009-08-17 12:39:25 +0800

    Reparent StopManager from none to SearchingManager

--- gogogo/media/StopManager.js
+++ gogogo/media/StopManager.js
@@ -10,11 +10,7 @@
  */
 
 gogogo.StopManager = function (map){
-	
-	/// The min zoom of stop markers
-	this.minZoom = 15;
-		
-	this.map = map;
+	gogogo.SearchingManager.call(this,map);	
 	
 	// Stop dictionary
 	this.stops = new Object();
@@ -22,30 +18,20 @@ gogogo.StopManager = function (map){
 	var options = { borderPadding: 50, trackMarkers: true };
 	
 	this.markermanager = new MarkerManager(map,options);
-	
-	manager = this;
 
-	GEvent.addListener(map, "moveend", function(){
-		manager.refresh();
-	});	
-	
 };
+
+extend(gogogo.StopManager,gogogo.SearchingManager);
 
 /** Refresh the stop list from gogogo server.
  * 
  */
 
-gogogo.StopManager.prototype.refresh = function() {
-	
-	zoom = this.map.getZoom();
-	
-	if (zoom < this.minZoom)
-		return;	
-	
-	bounds = this.map.getBounds();
-	api = "/api/stop/search/" + 
-		bounds.getNorthEast().lat() + "," + bounds.getNorthEast().lng() + ","	+
-		bounds.getSouthWest().lat() + "," + bounds.getSouthWest().lng();
+gogogo.StopManager.prototype.refresh = function(bounds) {	
+
+	var api = "/api/stop/search/" + 
+		bounds.getSouthWest().lat() + "," + bounds.getSouthWest().lng() + "," +
+		bounds.getNorthEast().lat() + "," + bounds.getNorthEast().lng();
 	
 	manager = this;
 	
