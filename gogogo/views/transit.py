5a0920c00f9e7544bfae2e323fa4fc2479ab51c2
Ben Lau <xbenlau@gmail.com>
2009-09-08 01:47:50 +0800

    [Bug fix] Agency page can not be shown

--- gogogo/views/transit.py
+++ gogogo/views/transit.py
@@ -87,7 +87,7 @@ def agency(request,agency_id):
     """
     
     agency_loader = AgencyLoader(agency_id)
-    agency_loader.load(request)
+    agency_loader.load()
 
     agency = trEntity(agency_loader.get_agency_entity(),request)
     agency["type"] = TransitTypeProperty.get_type_name(agency["type"])
