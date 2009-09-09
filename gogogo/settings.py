83121e2011fb681a34d35eac70f749db66aab3fd
vincicat <vinci@vinci-laptop.(none)>
2009-09-09 15:00:00 +0800

    N-in-1 commit: 1. ie 6 upgrade script (not tested) 2. Tidy up transit information 3. implemented jQeuryUI theme (see transit information for the effect...not tested in IE) 4. Route Search box hint 5. Route Search Result tidy up 6. base template html error fix

--- gogogo/settings.py
+++ gogogo/settings.py
@@ -16,13 +16,15 @@ gogogo.DEFAULT_LOCATION = [%f,%f];
 
 settings.add_app_media('combined-%(LANGUAGE_CODE)s.js',
     setting_converter,
+    'gogogo/jquery-ui-1.7.1.custom.min.js',
 )
 settings.add_app_media('combined-plugins.js',
-	'gogogo/jquery.treeview.min.js'
+	'gogogo/jquery.treeview.min.js',
+	'gogogo/jquery.hint.js',
+	'gogogo/jquery.button.js'
 )
 settings.add_app_media('combined-%(LANGUAGE_CODE)s-gogogo.js',
     
-    'gogogo/jquery-ui-1.7.1.custom.min.js',
     'gogogo/jquery.layout.js',
     #Marker Manager v1.1  
     # http://gmaps-utility-library.googlecode.com/svn/trunk/markermanager/1.1/src/markermanager_packed.js
@@ -59,5 +61,5 @@ settings.add_app_media('combined-%(LANGUAGE_DIR)s.css',
     # ...
 )
 settings.add_app_media('combined-plugins.css',
-	'gogogo/jquery.treeview.css',
+	'gogogo/jquery.treeview.css'
 )
